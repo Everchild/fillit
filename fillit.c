d8ea4cfee114136fa579410320c86a7960015deb
Déborah <deborah.recourse@hotmail.fr>
2017-02-01 23:46:27 +0100

    backtracking --> commentaire

--- fillit.c
+++ fillit.c
@@ -12,33 +12,82 @@
 
 #include <fillit.h>
 
-/*static void			ft_create_map(t_env *env)
+/*static void			ft_create_map(t_env *env, int nb_caract)
 {
-	char			*map;
-	int				side;
 	int				i;
-	int				nb_caract;
 
 	i = -1;
-	side = ft_sqrt(nb_tetri * 4);
-	nb_caract = side * (side + 1);
-	if (!(map = (char *)ft_memalloc(sizeof(char) * (nb_caract + 1))))
-		return (NULL);
 	while (++i < nb_caract)
 	{
-		if (i == side || (i > side && (i - side) % (side + 1) == 0))
-			map[i] = '\n';
+		if (i == t_env->side || (i > t_env->side && (i - t_env->side) % (t_env->side + 1) == 0))
+			t_env->map[i] = '\n';
 		else
-			map[i] = '.';
+			t_env->map[i] = '.';
 	}
-	map[i] = '\0';
-	return (map);
-}*/
+}
+
+static void	ft_remove_tetri(t_env *env, t_tetri tetri, int nb_caract)
+{
+	int		i;
+
+	i = 0;
+	while (i < nb_caract)
+	{
+		if (env->map[i] == tetri.letter)
+			env->map[i] = '.';
+		i++;
+	}
+}
+
+static bool	ft_backtrack(t_env *env, int nb_caract, int position, int i)
+{
+	t_tetri		tetri;
+
+	tetri = env->tetris[i];
+	if (i >= nb_tetri)
+		return (true);
+	while (position < nb_caract)
+	{
+		if (tetri.type.place_tetri(t_env->map, position, tetri.letter, t_env->side) == TETRI_PLACED)
+		{
+			position++;
+			i++;
+			if (ft_backtrack(env, position, i))
+				return true;
+		}
+		position++;
+		if (t_env->map[position] == '\n')
+			position++;
+	}
+	ft_remove_tetri(env, tetri, nb_caract);
+	return (false);
+}
+
+static void	ft_complete_map(t_env *env)
+{
+	int				nb_caract;
+
+	t_env->side = ft_sqrt(t_env->nb_tetri * 4);
+	nb_caract = t_env->side * (t_env->side + 1);
+	if (!(t_env->map = (char *)ft_memalloc(sizeof(char) * (nb_caract + 1))))
+		return (NULL);
+	ft_create_map(env, nb_caract);
+	while (ft_backtrack(env, nb_caract, 0, 0) == false)  //1er 0 = position 2eme = tetri
+	{
+		t_env->side++;
+		free t_env->map;
+		if (!(t_env->map = (char *)ft_memalloc(sizeof(char) * ((t_env->side * (t_env->side + 1)) + 1))))
+			return (NULL);
+		ft_create_map(env);
+	}
+}
+*/
 
 void				fillit(t_env *env)
 // sert uniquement à afficher les tetris avec leurs lettres, tu peux tout supprimer si besoin
 {
-//	ft_create_map(env);
+//	ft_complete_map(env);
+//	ft_putstr(env->map);
 	int				i;
 	int				j;
 	t_tetri			tetri;
