d7ccc0f309a9b7ee708423c0f1fec66308435598
Everchild <sarah.justine.brochard@gmail.com>
2017-02-02 19:40:44 +0100

    last little improvements

--- fillit.c
+++ fillit.c
@@ -61,60 +61,23 @@ static t_bool	ft_backtrack(t_env *env, int nb_caract, int i)
 	return (FALSE);
 }
 
-static void	ft_complete_map(t_env *env)
+void				fillit(t_env *env)
 {
 	int				nb_caract;
 
 	env->side = ft_sqrt(env->nb_tetris * 4);
 	nb_caract = env->side * (env->side + 1);
 	if (!(env->map = (char *)ft_memalloc(sizeof(char) * (nb_caract + 1))))
-		ft_exit("error: failed to allocate memory", FAILED_TO_MALLOC);
+		ft_exit("error", FAILED_TO_MALLOC);
 	ft_create_map(env, nb_caract);
 	while (ft_backtrack(env, nb_caract, 0) == FALSE)
 	{
 		env->side++;
 		ft_memdel((void **)&env->map);
 		if (!(env->map = (char *)ft_memalloc(sizeof(char) * ((env->side * (env->side + 1)) + 1))))
-			ft_exit("error: failed to allocate memory", FAILED_TO_MALLOC);
+			ft_exit("error", FAILED_TO_MALLOC);
 		nb_caract = env->side * (env->side + 1);
 		ft_create_map(env, nb_caract);
 	}
-}
-
-
-void				fillit(t_env *env)
-// sert uniquement à afficher les tetris avec leurs lettres, tu peux tout supprimer si besoin
-{
-	ft_complete_map(env);
 	ft_putstr(env->map);
-/*
-	int				i;
-	int				j;
-	t_tetri			tetri;
-	char			*str;
-	int				index;
-
-	i = 0;
-	while (i < env->nb_tetris)
-	{
-		tetri = env->tetris[i];
-		str = ft_memalloc(sizeof(char) * (20 + 1));
-		j = 0;
-		while (j < 20)
-		{
-			str[j] = '.';
-			if (j == 4 || j == 9 || j == 14 || j == 19)
-				str[j] = '\n';
-			j++;
-		}
-		index = 0;
-		while (tetri.type.place_tetri(str, index, tetri.letter, 4) == INVALID_PLACEMENT)
-			index++;
-		ft_putstr(str);
-		i++;
-		if (i < env->nb_tetris)
-			ft_putchar('\n');
-		ft_memdel((void **)&str);
-	}
-*/
 }
